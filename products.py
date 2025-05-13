7a84d73687b893e3ca95c904e4d20eba0f936a1d
RaquelgLeyva <raquelgleyva@gmail.com>
2025-05-13 10:34:58 +0200

    Create Product class

---
+++ products.py
@@ -0,0 +1,53 @@
+
+class Product:
+
+    def __init__(self, name, price, quantity):
+
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        if price < 0:
+            raise ValueError("Price cannot be less than 1 or a negative number.")
+        if quantity < 0:
+            raise ValueError("Quantity cannot be less than 1 or a negative number.")
+        self.name = name #str
+        self.price = price #float
+        self.quantity = quantity #int
+        self.active = True #bool
+
+    def get_quantity(self) -> int:
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
+        self.quantity = quantity
+        if quantity == 0:
+            self.deactivate()
+
+    def is_active(self) -> bool:
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self) -> str:
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity) -> float:
+        if not self.active:
+            raise Exception("Product is not active.")
+        if quantity <= 0:
+            raise ValueError("Quantity must be greater than zero.")
+        if quantity > self.quantity:
+            raise Exception("Not enough quantity in stock.")
+
+        self.quantity -= quantity
+        if self.quantity == 0:
+            self.deactivate()
+        return self.price * quantity
+
+
+
